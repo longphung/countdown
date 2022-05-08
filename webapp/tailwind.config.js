663a7fed9c6f799d11e605b0cb2eece38795034c
Phung Nguyen <longphung.nn@gmail.com>
2022-05-09 02:34:09 +0700

    ✨ feat: add react router

---
+++ webapp/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{ts,tsx}"],
+  theme: {
+    fontFamily: {
+      sans: ["Montserrat", "sans-serif"],
+      serif: ["Georgia", "serif"],
+      mono: ["Menlo", "monospace"],
+      cursive: ["Oleo Script Swash Caps", "cursive"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
